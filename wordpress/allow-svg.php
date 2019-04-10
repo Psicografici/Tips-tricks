7aed198e8004d205aea63f3956f9191de4b04494
Tiex84 <psicografici@gmail.com>
2019-04-11 01:18:52 +0200

    allow svg file

---
+++ wordpress/allow-svg.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+ * Allow SVG file to Media livbrary
+ * Paste this code into your function.php file 
+ */
+
+function cc_mime_types($mimes) {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
+
+?>
